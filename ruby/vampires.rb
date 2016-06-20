1f31a8414c4d2fce4c7ac9557d0baba688676eaf
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-19 19:36:23 -0700

    Add loop for processing multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,28 +1,37 @@
+puts "How many employees will you be processing?"
+employees = gets.chomp.to_i
+counter = 0
 currentYear = 2016
-puts "What is your name?"
-name = gets.chomp
 
-puts "How old will you be after your birthday this year?"
-# Weird phrasing to get around asking for current date
-age = gets.chomp.to_i
+while counter < employees
+  counter += 1
 
-puts "What year were you born?"
-birthYear = gets.chomp.to_i
+  puts "What is your name?"
+  name = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlicBread = gets.chomp
+  puts "How old will you be after your birthday this year?"
+  # Weird phrasing to get around asking for current date
+  age = gets.chomp.to_i
 
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-insurance = gets.chomp
+  puts "What year were you born?"
+  birthYear = gets.chomp.to_i
 
-if name == 'Drake Cula' || name == 'Tu Fang'
-  puts "Definitely a vampire"
-elsif (!(age == currentYear - birthYear) && garlicBread == 'n') && insurance == 'n'
-  puts "Almost certainly a vampire"
-elsif !(age == currentYear - birthYear) && (garlicBread == 'n' || insurance == 'n')
-  puts "Probably a vampire"
-elsif (age == currentYear - birthYear) && (garlicBread == 'y' || insurance == 'y')
-  puts "Probably not a vampire"
-else
-  puts "Results inconclusive"
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+  garlicBread = gets.chomp
+
+  puts "Would you like to enroll in the company's health insurance? (y/n)"
+  insurance = gets.chomp
+
+  if name == 'Drake Cula' || name == 'Tu Fang'
+    puts "Definitely a vampire"
+  elsif (!(age == currentYear - birthYear) && garlicBread == 'n') && insurance == 'n'
+    puts "Almost certainly a vampire"
+  elsif !(age == currentYear - birthYear) && (garlicBread == 'n' || insurance == 'n')
+    puts "Probably a vampire"
+  elsif (age == currentYear - birthYear) && (garlicBread == 'y' || insurance == 'y')
+    puts "Probably not a vampire"
+  else
+    puts "Results inconclusive"
+  end
+
 end
