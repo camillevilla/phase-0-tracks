e93a61c71067536cef0ae0b0bb9c7807fa19bc23
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-19 21:05:31 -0700

    Add driver code to secret_agents.rb

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,5 +1,4 @@
 
-
 # encrypt
 #advance each string of a letter one space forward, i.e. abc -> bcd
 # set up a loop
@@ -52,10 +51,30 @@ def decrypt(x)
   return x
 end
 
-puts encrypt('abc')
-puts encrypt('zed')
-puts decrypt("bcd")
-puts decrypt("afe")
-puts decrypt(encrypt("swordfish"))
+#DRIVER CODE
+#ask: decrypt or encrypt password?
+puts "Would you like to encrypt or decrypt your password?"
+response = gets.chomp
+
+#ask for a password
+puts "What is your password?"
+password = gets.chomp
+
+#run method, print result, exit
+if response == "encrypt"
+  puts encrypt(password)
+elsif response == "decrypt"
+  puts decrypt(password)
+else
+  puts "please enter a valid command"
+end
+
+
+#tests
+#puts encrypt('abc')
+#puts encrypt('zed')
+#puts decrypt("bcd")
+#puts decrypt("afe")
+#puts decrypt(encrypt("swordfish"))
 #The encrypt and decrypt methods are two sides of the same coin. encrypt("swordfish") returns "txpsegjti". This result is then read by decrypt.
 #The nested method call is equivalent to decrypt("txpsegjti").
