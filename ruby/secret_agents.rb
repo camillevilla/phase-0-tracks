4e3ba2f6cf9ee688b440973dc2689b3b92745baf
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-19 20:52:01 -0700

    Add decryption program to secret_agents.rb

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -26,11 +26,6 @@ def encrypt(x)
   puts x
 end
 
-encrypt('abc')
-encrypt('zed')
-#puts decrypt("bcd")
-#puts decrypt("afe")
-
 #decrypt, i.e. bcd -> abc
 #move password letters back one letter
 #set up a loop
@@ -39,3 +34,25 @@ encrypt('zed')
   # look at the letter, get its index
     # if a, change to z
     # else change letter to alphabet[index - 1]
+
+def decrypt(x)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  counter = 0
+  while counter < x.length
+    if x[counter] != ' '
+      if x[counter] == 'a'
+        x[counter] = 'z'
+      else
+        letterIndex = alphabet.index(x[counter])
+        x[counter] = alphabet[letterIndex - 1]
+      end
+    end
+    counter += 1
+  end
+  puts x
+end
+
+encrypt('abc')
+encrypt('zed')
+puts decrypt("bcd")
+puts decrypt("afe")
