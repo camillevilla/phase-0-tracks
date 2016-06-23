92d7eef1b488ad6aa249b974eb0c8d67328e3657
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-23 12:02:53 -0700

    Add breakfast array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,19 @@ def test_method
 end
 
 test_method {3.times{|x| puts "This is happening #{x} times"}}
+
+breakfast = ["hash browns", "eggs", "bacon"]
+second_breakfast = {pancakes: "silver dollar", frenchtoast: "sour dough french toast"}
+
+breakfast.each {|item| puts "We are having #{item} for breakfast!"}
+p breakfast
+
+breakfast.map! do |item|
+  item.upcase
+end
+p breakfast
+
+second_breakfast.each do |item|
+  puts item
+end
+p second_breakfast
