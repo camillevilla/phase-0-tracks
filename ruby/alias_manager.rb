db107a81d607c148ddbb02832bf889610adb6437
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-24 00:01:26 -0700

    Separate letter swapping and name generation into different methods

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,49 +1,54 @@
 #method that takes a spy's real name and creates a fake name
-def secret_name()
-  #Ask for name
-  puts "First name: "
-  first_name = gets.chomp
-  puts "Last name: "
-  last_name = gets.chomp
 
-  #Swap first and last name
-  new_name = last_name + " " + first_name
-  new_name = new_name.chars
-
+#method for shifting vowels and consonants
+def letter_swap(phrase)
   #set up vowel and consonant arrays
-  vowels = 'aeiou'
-  vowels = vowels + vowels.upcase
-  vowels = vowels.chars
-  consonants = 'bcdfghjklmnpqrstvwxyz'
-  consonants = consonants + consonants.upcase
-  consonants = consonants.chars
+  vowels = 'aeiou'.chars
+  consonants = 'bcdfghjklmnpqrstvwxyz'.chars
   counter = 0
 
-  #Loop through each letter
-  while counter < new_name.length
-    letter = new_name[counter]
+  #convert string to array
+  phrase = phrase.downcase.chars
 
+  #Loop through each letter
+  while counter < phrase.length
+    letter = phrase[counter]
   #Change all vowels to the next vowel in 'aeiou'
     if vowels.include?(letter)
-      new_name[counter] = vowels[vowels.find_index(letter) + 1]
+      phrase[counter] = vowels[vowels.find_index(letter) + 1]
 
   #Change all consonants to the next consonant
     elsif consonants.include?(letter)
-      new_name[counter] = consonants[consonants.find_index(letter) + 1]
+      phrase[counter] = consonants[consonants.find_index(letter) + 1]
     end
   counter += 1
   end
 
-  new_name = new_name.join
-  p new_name
+  #rejoin strings
+  phrase.join
+
+end
+
+def secret_name()
+  #Ask for name
+  puts "First name: "
+  first_name = gets.chomp.downcase
+  puts "Last name: "
+  last_name = gets.chomp.downcase
+
+  #run letterswap on both names
+  first_name = letter_swap(first_name)
+  last_name = letter_swap(last_name)
+  #Swap first and last name
+  new_name = last_name.capitalize + " " + first_name.capitalize
+  puts "\nYour new name is #{new_name}."
 end
 
 #User prompt
 puts "Welcome to the secret name generator! Press Enter to proceed. Type 'quit' to exit the program."
-secret_name
-input = gets.chomp
+input = ''
 while input != "quit"
-  input = gets.chomp
-  secret_name()
+  secret_name
   puts "Press Enter to generate a new name. Type 'quit' to exit."
+  input = gets.chomp
 end
