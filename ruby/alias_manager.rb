093d6beac7d2bb240be5fbb74f7e38e5cd7c1a53
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-23 23:10:45 -0700

    Add method for generating spy names

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,42 @@
+#method that takes a spy's real name and creates a fake name
+
+def secret_name()
+  #Ask for name
+  puts "First name: "
+  first_name = gets.chomp
+  puts "Last name: "
+  last_name = gets.chomp
+
+  #Swap first and last name
+  new_name = last_name + " " + first_name
+  new_name = new_name.chars
+
+  #set up vowel and consonant arrays
+  vowels = 'aeiou'
+  vowels = vowels + vowels.upcase
+  vowels = vowels.chars
+  consonants = 'bcdfghjklmnpqrstvwxyz'
+  consonants = consonants + consonants.upcase
+  consonants = consonants.chars
+  counter = 0
+
+  #Loop through each letter
+  while counter < new_name.length
+    letter = new_name[counter]
+
+  #Change all vowels to the next vowel in 'aeiou'
+    if vowels.include?(letter)
+      new_name[counter] = vowels[vowels.find_index(letter) + 1]
+
+  #Change all consonants to the next consonant
+elsif consonants.include?(letter)
+      new_name[counter] = consonants[consonants.find_index(letter) + 1]
+    end
+  counter += 1
+  end
+
+  new_name = new_name.join
+  p new_name
+end
+
+secret_name()
