639f68b335415eca38deaa76319076ec7020bc84
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-27 17:53:18 -0700

    Defined some puppy behaviors

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,30 @@
 class Puppy
-
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(int)
+    int.times {|x| puts "Woof!"}
+  end
+
+  def rollover
+    puts "Roll over!"
+  end
+
+  def dog_years(int)
+    human_years = int * 7
+    p human_years
+  end
+
+  def shake_paw
+    p "Shakes paw."
+  end
 end
 
+fido = Puppy.new
+fido.fetch('stick')
+fido.speak(3)
+fido.rollover
+fido.dog_years(3)
+fido.shake_paw
