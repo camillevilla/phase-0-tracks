064eabe3adabc9ff76be53208074e89045656b8d
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-27 22:02:59 -0700

    Add loop for generating Santa instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,17 +1,33 @@
 class Santa
   def speak
-    "Ho, ho, ho! Haaaappy holidays!"
+    puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie} cookie!"
   end
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
+
+  def state
+    puts "I am #{@gender} #{@ethnicity}"
+
 end
 
-st_nick = Santa.new
-st_nick.speak
-st_nick.eat_milk_and_cookies("chocolate chip")
+santas = []
+example_genders = ["two-spirit", "female", "trans female", "trans male", "cis male"]
+example_ethnicities = ["Czech", "Chinese", "Japanese", "Mexican", "Pakistani"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+santas.each do |x|
+  x.state
+end
+end
