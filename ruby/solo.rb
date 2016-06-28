846f63944ee35e4b7ddca324fd6008dc0a0be33e
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-28 09:58:24 -0700

    Add dragon class design to solo.rb

---
+++ ruby/solo.rb
@@ -0,0 +1,10 @@
+#class: Dragon
+  #attributes
+    #age - int
+    #element - string
+    #horde - number of gold pieces
+
+  #behaviors
+    #rawr(int) - print "RAWR" specified number of times
+    #add_to_horde
+    #flame
