be833022d3d30912e0091f2d2398141703f14a9d
cvilla100@gmail.com <cvilla100@gmail.com>
2016-06-29 13:55:12 -0700

    Change Shout to a mixin, shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,33 @@
+#Release 3: convert standalone module to a mixin
+module Shout
+  def yell_angrily(words)
+    puts words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+    puts words.upcase + "de da! :)"
+  end
+end
+
+class Deer
+  include Shout
+end
+
+class Old
+  include Shout
+end
+
+deer = Deer.new
+deer.yell_angrily("Oorah!")
+deer.yelling_happily("Grass")
+
+old = Old.new
+old.yell_angrily("Get off my lawn")
+old.yelling_happily("prunes")
+
+
+#Release 1 code
+=begin
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -10,3 +40,4 @@ end
 
 p Shout.yell_angrily("ARGH")
 p Shout.yelling_happily("la")
+=end
