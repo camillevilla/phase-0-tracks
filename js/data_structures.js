931b64624ba26a29a9a82f90697e70091eaca824
cvilla100@gmail.com <cvilla100@gmail.com>
2016-07-15 15:06:01 -0700

    Add Car constructor function to data_structures.js

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,7 +10,28 @@ for (var i = 0; i < colors.length; i++){
   stable[names[i]] = colors[i];
 }
 
+//Car constructor function
+function Car(model,color) {
+  console.log("A new car, rolling off the assembly line...");
+
+  this.model = model;
+  this.color = color;
+
+  this.honk = function(){
+    console.log("BLAAAAAAARP BLAAAARP");
+  }
+
+}
+
 // DRIVER CODE
 console.log(colors);
 console.log(names);
 console.log(stable);
+
+  //Testing out constructor function
+  var jetson = new Car("flying", "purple");
+  jetson.honk();
+  console.log(jetson.color);
+
+  var flinstones = new Car("rock-powered", "orange");
+  console.log(flinstones.model);
