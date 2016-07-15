af3b659a214d97b514539ee9c122281e6ce37a57
cvilla100@gmail.com <cvilla100@gmail.com>
2016-07-15 14:23:20 -0700

    Add data_structures.js

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["mauve", "aubergine", "maroon", "peach"];
+var names = ["Rainicorn","Pinky Puff","Sparkles","Bucephalus"];
+
+colors.push("turquoise");
+colors.push("Gibbs");
+
+// DRIVER CODE
+console.log(colors);
+console.log(names);
