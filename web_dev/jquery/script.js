0038a98f37b7454e65a658b73d81c12834ebecfe
cvilla100@gmail.com <cvilla100@gmail.com>
2016-07-27 12:43:09 -0700

    Remove deselected items from receipt

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,8 +1,6 @@
 //Notes
-//- hide elements responding to click events
-//- figure out how to bundle two actions into one click event (e.g. clicking "pepperoni" affects the button AND adds pepperoni to the receipt table)
-//- learn how to make things return to their previous state after being clicked
-//-how to traverse the DOM: clicking on a header affects CSS properties of child ul tags
+//
+// - fix Toppings toggle problems
 
 // hide lists by default
 $("ul").addClass("hidden");
@@ -29,20 +27,11 @@ $("li").on('click',function(){
   x = $(this).text();
     //need to use a .each to check if ANY tr matches the de-selected text
     if (check_receipt(x) == true)
-     $("#receipt").find("tr").last().remove("tr");
+     remove_receipt_row(receipt_row(x));
   else
    $("table").append("<tr><td>"+ x + "</td></tr>");
 });
 
-//testing out .each
-//DO NOT CHANGE - this prints out <li></li>
-$.each($("li"), function(index,value){console.log(value);});
-
-//DO NOT CHANGE - access contents of li, prints out topping string
-$.each($("li"), function(index,value){console.log($(value).text());});
-
-//challenge: iterate through each item in the receipt table
-$.each($("#receipt").find("td"), function(index,value){console.log($(value).text());});
 
 //returns true if any receipt values matches given string
 function check_receipt(x){
@@ -58,13 +47,25 @@ function check_receipt(x){
   return false;
 };
 
-//Refactor to two functions
-  //find match
-  //return matching receipt row
+//return matching receipt row
+  function receipt_row(x){
+    search = $("#receipt").find("td");
+    stop = search.length
+    var current_text = '';
+    for (var i = 0; i < stop; i++){
+      current_text = $(search[i]).text();
+      if (current_text == x){
+        return i;
+      }
+    }
+    return false;
+  };
+
+function remove_receipt_row(int){
+$("#receipt tr:nth-child("+ (int + 1) + ")").remove();
+};
 
 console.log(check_receipt("wee"));
+console.log(receipt_row("wee"));
+console.log(receipt_row("ya"));
 console.log(check_receipt("wah"));
-
-// DO NOT CHANGE - example of removing nth child <-- eventually plug in index from find_row
-$("#crust li:nth-child(2)").remove();
-($("#crust").children()).indexOf("Stuffed");
