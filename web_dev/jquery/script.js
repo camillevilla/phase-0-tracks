9e6c164a61afacfafe5be4528854065e830496c4
cvilla100@gmail.com <cvilla100@gmail.com>
2016-07-27 12:18:19 -0700

    Add selected topping to receipt IFF it isn't there

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -4,19 +4,19 @@
 //- learn how to make things return to their previous state after being clicked
 //-how to traverse the DOM: clicking on a header affects CSS properties of child ul tags
 
-
-// hide or show sub-ul if the h3 is clicked. does it need a child tag?
-
 // hide lists by default
 $("ul").addClass("hidden");
+$("h4").addClass("hidden");
 
 $("h3").on('click',function(){
   //remove state un-selected elements
-  $(".selected").next().addClass("hidden");
+  $(".selected").next("ul").addClass("hidden");
   $(".selected").removeClass("selected");
 
-  //add state to selected elements
+
   $(this).next().toggleClass("hidden");
+
+  //add state to selected elements
   $(this).toggleClass("selected");
 
 });
@@ -26,6 +26,45 @@ $("h3").on('click',function(){
 var x ='';
 $("li").on('click',function(){
   $(this).toggleClass("selected");
-  x = $("li.selected").text();
-  $("table").append("<tr><td>"+ x + "</td></tr>");
+  x = $(this).text();
+    //need to use a .each to check if ANY tr matches the de-selected text
+    if (check_receipt(x) == true)
+     $("#receipt").find("tr").last().remove("tr");
+  else
+   $("table").append("<tr><td>"+ x + "</td></tr>");
 });
+
+//testing out .each
+//DO NOT CHANGE - this prints out <li></li>
+$.each($("li"), function(index,value){console.log(value);});
+
+//DO NOT CHANGE - access contents of li, prints out topping string
+$.each($("li"), function(index,value){console.log($(value).text());});
+
+//challenge: iterate through each item in the receipt table
+$.each($("#receipt").find("td"), function(index,value){console.log($(value).text());});
+
+//returns true if any receipt values matches given string
+function check_receipt(x){
+  search = $("#receipt").find("td");
+  stop = search.length
+  var current_text = '';
+  for (var i = 0; i < stop; i++){
+    current_text = $(search[i]).text();
+    if (current_text == x){
+      return true;
+    }
+  }
+  return false;
+};
+
+//Refactor to two functions
+  //find match
+  //return matching receipt row
+
+console.log(check_receipt("wee"));
+console.log(check_receipt("wah"));
+
+// DO NOT CHANGE - example of removing nth child <-- eventually plug in index from find_row
+$("#crust li:nth-child(2)").remove();
+($("#crust").children()).indexOf("Stuffed");
